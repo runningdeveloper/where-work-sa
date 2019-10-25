c653d9c747aaef9e9e8b13f660c521a8308a76a5
Geoffrey <info@runningdeveloper.com>
2019-10-25 20:21:43 +0200

    added the seo image

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,10 +23,10 @@ module.exports = {
         name: `where work sa`,
         short_name: `where work`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#5B79A4`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/south-africa.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
