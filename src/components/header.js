d6b1a3b1efa6babcc18cbdf155da9e99a7ceb4c0
locemarn <marcelon27@gmail.com>
2019-10-25 19:35:19 -0300

    fix eslint warnings

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState, useEffect } from "react"
+import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header>
